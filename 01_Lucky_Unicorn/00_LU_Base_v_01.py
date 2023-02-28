02e4272b0588a2de9255f2483bf107db3678e769
narayanj1193 <101072172+narayanj1193@users.noreply.github.com>
2023-02-28 13:29:49 +1300

    Add files via upload

--- 01_Lucky_Unicorn/00_LU_Base_v_01.py
+++ 01_Lucky_Unicorn/00_LU_Base_v_01.py
@@ -17,15 +17,33 @@ def yes_no(question):
 
         # If they say no, output 'display instructions'
         else:
-            print("Please answer yes / no")
+            print("Please answer yes / no \n")
 
 
 def instructions():
-    print("*** How to Play ***")
-    print()
-    print("These are the rules:")
-    print()
+    print("\n *** How to Play *** \n")
+    print("These are the rules:\n* stuff about rules *\n")
     return ""
+
+
+def num_checker(question, low, high):
+    # Main Routine
+    error = "Please enter a whole number that is between 1 and 10. \n"
+
+    while True:
+        try:
+            # Ask the question
+            response = int(input(question))
+
+            # if the amount is too low / too high give
+            if low < response <= high:
+                return response
+
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
 
 
 # Main routine
@@ -35,5 +53,7 @@ played_before = yes_no("Have you played this game before? ")
 if played_before == "no":
     instructions()
 
+# Ask user how much they want to play with
+betting_amount = num_checker("How much would you like to play with? ", 0, 10)
 
-print()
+print("You have chosen to spend ${} \n".format(betting_amount))
